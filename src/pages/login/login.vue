68404f0dae24b28ae5e884d1e669211405b8d441
johanchow <johanchow@163.com>
2024-10-16 07:06:35 +0800

    登录页优化展示

--- src/pages/login/login.vue
+++ src/pages/login/login.vue
@@ -1,8 +1,13 @@
 <template>
   <view>
     <!-- 检查是否已授权 -->
-    <view v-if="!hasAuth">
-      <button type="primary" @tap="getUserProfile">授权微信信息</button>
+    <view class="auth-container" v-if="!hasAuth">
+      <view class="corporation">
+        <image src="//wx.qlogo.cn/mmhead/ZqDaDiccbgkgIR2dMSQdSic91KghfoLMkk8geIoibR8N7zAzrke9EEkvyOqs078oneoNUu8knzwVHI/0" mode="widthFix" class="logo"/>
+        <text class="title">空中air试衣</text>
+      </view>
+      <view class="auth-tip">使用微信号一键登录(不获取微信信息，仅区分用户)</view>
+      <button type="primary" @tap="getUserProfile">授权</button>
     </view>
   </view>
 </template>
@@ -24,6 +29,7 @@ onBeforeMount(() => {
 });
 const getUserProfile = async () => {
   let userInfo: UserInfo;
+  console.log('开始调用uni.getUserProfile');
   try {
     // 使用获取到的code调用getUserProfile获取用户信息
     const { userInfo: info } = await uni.getUserProfile({
@@ -38,8 +44,12 @@ const getUserProfile = async () => {
       avatarUrl: info.avatarUrl,
       nickName: info.nickName,
     };
-  } catch (error) {
+  } catch (error: any) {
     console.error('获取微信用户信息失败:', error);
+    uni.showToast({
+      title: `微信登录失败: ${error.errno}`,
+      icon: 'none',
+    });
     return;
   }
   userInfoRef.value = userInfo;
@@ -61,6 +71,10 @@ const getIdentity = async () => {
   const res = await uni.login();
   if (!res.code) {
     console.error('登录失败！');
+    uni.showToast({
+      title: '登录失败，请退出重试',
+      icon: 'none',
+    })
     return;
   }
   const openIdRes: AnyObject = await uni.request({
@@ -72,6 +86,10 @@ const getIdentity = async () => {
   });
   if (!openIdRes.data?.openid) {
     console.error('获取openid失败:', openIdRes.data?.openid);
+    uni.showToast({
+      title: '获取openid失败',
+      icon: 'none',
+    });
     return;
   }
   console.log('获取identity成功:', openIdRes.data);
@@ -79,30 +97,37 @@ const getIdentity = async () => {
 };
 </script>
 
-<style>
-.content {
+<style lang="scss">
+.auth-container {
+  padding: 25rpx;
   display: flex;
   flex-direction: column;
+  justify-content: center;
   align-items: center;
-  justify-content: center;
-}
-
-.logo {
-  height: 200rpx;
-  width: 200rpx;
-  margin-top: 200rpx;
-  margin-left: auto;
-  margin-right: auto;
-  margin-bottom: 50rpx;
-}
-
-.text-area {
-  display: flex;
-  justify-content: center;
-}
-
-.title {
-  font-size: 36rpx;
-  color: #8f8f94;
+  min-height: 600rpx;
+  .corporation {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    gap: 24rpx;
+    image {
+      width: 100rpx;
+      height: 100rpx;
+      border-radius: 50rpx;
+    }
+    .title {
+      font-size: 36rpx;
+      color: #000
+    }
+  }
+  .auth-tip {
+    font-size: 24rpx;
+    margin-top: 20rpx;
+  }
+  button {
+    width: 600rpx;
+    margin-top: 200rpx;
+  }
 }
 </style>
